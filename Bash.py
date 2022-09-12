d7eb234e0a1c4c3b7c4febe4ee07ba07c42ed9b8
adhisashilfa <21083010016@student.upnjatim.ac.id>
2022-09-12 15:19:03 +0800

    Bash.py

---
+++ Bash.py
@@ -0,0 +1,12 @@
+print('-' *50)
+print('Tugas 1 SISOP Kelas-A')
+print("-" *50)
+print('Nama: Adhisa Shilfadianis Iffadah')
+print('NPM : 21083010016')
+print('-' *50)
+print('notes:')
+print('-' *50)
+print('Halo Dunia!')
+print('Ini adalah skrip Bash pertamaku di Linux')
+
+
